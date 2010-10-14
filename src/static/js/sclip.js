24c2ec42f5c228a3d90ab0b356421f877b9b6fe9
krablak <krablak@7b4a1042-a560-49a6-8a58-85ef7ed48359>
2010-10-14 17:45:35 +0000

    Odstraneni zbytecnosti
    git-svn-id: http://ays.unfuddle.com/svn/ays_clips/sclip/trunk@3 7b4a1042-a560-49a6-8a58-85ef7ed48359

--- src/static/js/sclip.js
+++ src/static/js/sclip.js
@@ -2,29 +2,6 @@
  * Initialize basic UC
  */
 function initUI(){
-	//Add image previews to all source links
-	$('div#src a').imgPreview({
-	    containerID: 'imgPreviewWithStyles',
-	    imgCSS: {
-	        // Limit preview size:
-	        height: 300
-	    },
-	    // When container is shown:
-	    onShow: function(link){
-	        // Animate link:
-	        $(link).stop().animate({opacity:0.4});
-	        // Reset image:
-	        $('img', this).stop().css({opacity:0});
-	    },
-	    // When image has loaded:
-	    onLoad: function(){
-	        // Animate image
-	        $(this).animate({opacity:1}, 300);
-	    },
-	    // When container hides: 
-	    onHide: function(link){
-	        // Animate link:
-	        $(link).stop().animate({opacity:1});
-	    }
-	});
+	
+
 }
