314a13dfc4e203b36c4d3e8fda1100fcf674cbdb
krablak <krablak@7b4a1042-a560-49a6-8a58-85ef7ed48359>
2010-10-19 20:50:32 +0000

    Zacatek dialogu s detailem.
    git-svn-id: http://ays.unfuddle.com/svn/ays_clips/sclip/trunk@29 7b4a1042-a560-49a6-8a58-85ef7ed48359

--- src/static/js/sclip.js
+++ src/static/js/sclip.js
@@ -2,6 +2,12 @@
  * Initialize basic UC
  */
 function initUI(){
-	
+}
 
+/**
+ * Displays modal dialog for clicked clip.
+ * @param clipId id of the clip.
+ */
+function showDetail(clipId){
+	$('#detail_'+clipId).modal({overlayClose:true});
 }
