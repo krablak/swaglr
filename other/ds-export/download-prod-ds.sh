5491e4937af790f6272df22704d8af20fc7c9a58
krablak <krablak@7b4a1042-a560-49a6-8a58-85ef7ed48359>
2010-11-01 21:43:10 +0000

    Mac skripty na export z prod. a import do devel.
    git-svn-id: http://ays.unfuddle.com/svn/ays_clips/sclip/trunk@74 7b4a1042-a560-49a6-8a58-85ef7ed48359

---
+++ other/ds-export/download-prod-ds.sh
@@ -0,0 +1,7 @@
+echo Moving old data to temp file
+rm -rf ./ds-download/tmp/*
+mv ./ds-download/* ./ds-download/tmp/
+echo Starting data download...
+/usr/local/bin/appcfg.py download_data --config_file=bulkloader.yaml --filename=./ds-download/swagclip_export_clip.csv --kind=Clip --url=http://swagclip.appspot.com/_ah/remote_api
+/usr/local/bin/appcfg.py download_data --config_file=bulkloader.yaml --filename=./ds-download/swagclip_export_user.csv --kind=UserInfo --url=http://swagclip.appspot.com/_ah/remote_api
+/usr/local/bin/appcfg.py download_data --config_file=bulkloader.yaml --filename=./ds-download/swagclip_export_image.csv --kind=Image --url=http://swagclip.appspot.com/_ah/remote_api
---
+++ other/ds-export/upload-dev-ds.sh
@@ -0,0 +1,3 @@
+appcfg.py upload_data --config_file=bulkloader.yaml --filename=./ds-download/swagclip_export_user.csv --kind=UserInfo --url=http://localhost:8080/_ah/remote_api ../../src/
+appcfg.py upload_data --config_file=bulkloader.yaml --filename=./ds-download/swagclip_export_image.csv --kind=Image --url=http://localhost:8080/_ah/remote_api ../../src/
+appcfg.py upload_data --config_file=bulkloader.yaml --filename=./ds-download/swagclip_export_clip.csv --kind=Clip --url=http://localhost:8080/_ah/remote_api ../../src/
