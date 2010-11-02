168a93b146ce90cb8b3ebb18a034aa28a10be548
krablak <krablak@7b4a1042-a560-49a6-8a58-85ef7ed48359>
2010-11-02 20:07:16 +0000

    #10
    git-svn-id: http://ays.unfuddle.com/svn/ays_clips/sclip/trunk@77 7b4a1042-a560-49a6-8a58-85ef7ed48359

--- src/image.py
+++ src/image.py
@@ -7,17 +7,22 @@ from google.appengine.ext import db
 from google.appengine.api import urlfetch
 from google.appengine.api import images
 from dbo import Image
+import logging
 
 TINY_IMAGE_HEIGHT = 260;
 TINY_IMAGE_WIDTH = 260;
 
 def thumbnail(url=None):
+    logging.debug("Thumbnail start")
     image_do = Image()
     image_do.url = url
     image = __downloadImage(url)
     if image:
         __to_thumbnail(image,image_do)
+    else:
+        logging.debug("Image was not downloaded.")
     image_do.put()
+    logging.debug("Thumbnail end")
     return image_do
 
 def __downloadImage(url=None):
@@ -33,22 +38,25 @@ def __downloadImage(url=None):
 
 def __to_thumbnail(image,image_do):
     img_tiny = images.Image(image)
+    __log_image(img_tiny)
     resized = False
     #Check if the image is large and should be resized
     if __is_large(img_tiny):
         #Vertical image resize by height
         if __is_vertical(img_tiny):
             #img_tiny.resize(height=TINY_IMAGE_HEIGHT)
-            #resized = True
-            pass
+            img_tiny.resize(width=TINY_IMAGE_WIDTH)
+            resized = True
         #Horizontal image resize by width    
         elif __is_horizontal(img_tiny):
             img_tiny.resize(width=TINY_IMAGE_WIDTH)
             resized = True
+    else:
+        logging.debug("Image was is not large and will be not resized.")
     if resized:
         tiny = img_tiny.execute_transforms(output_encoding=images.JPEG)
         image_do.tiny = db.Blob(tiny)
-    else:
+    else:        
         image_do.tiny = db.Blob(image)
     
 def __is_large(image):
@@ -68,4 +76,10 @@ def __is_horizontal(image):
     Helper method checks if the image should by resized by width.
     """
     return image.width > image.height
+
+def __log_image(image):
+    """
+    Helper method for logging all details about processe image.
+    """
+    logging.debug("Image size %s x %s " % (image.height,image.width))
 
