bb907a7d51ff7f2ba972b1c76bd243992e143490
krablak <krablak@7b4a1042-a560-49a6-8a58-85ef7ed48359>
2010-11-09 22:54:22 +0000

    #22
    git-svn-id: http://ays.unfuddle.com/svn/ays_clips/sclip/trunk@114 7b4a1042-a560-49a6-8a58-85ef7ed48359

--- test/tests/base.py
+++ test/tests/base.py
@@ -17,12 +17,14 @@ class TestLocalHost(unittest.TestCase):
     def test_valid_load(self):
         test_data = testutils.load_data("data/valid.csv")
         for data in test_data:
+            print "Post data : %s" %  (data)
             res = swaglr.api.post(params=data,verbose=True)
             self.assertEquals('OK',res['code'])
 
     def test_invalid_load(self):
         test_data = testutils.load_data("data/invalid.csv")
         for data in test_data:
+            print "Post data : %s" %  (data)
             res = swaglr.api.post(params=data,verbose=True)
             self.assertEquals('ERROR',res['code'])
 
