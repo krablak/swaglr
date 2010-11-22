2b394089e9a3b0befe83d35b8451d22f19449d55
krablak <krablak@7b4a1042-a560-49a6-8a58-85ef7ed48359>
2010-11-22 21:28:36 +0000

    #30
    git-svn-id: http://ays.unfuddle.com/svn/ays_clips/sclip/trunk@167 7b4a1042-a560-49a6-8a58-85ef7ed48359

--- src/ui/routing.py
+++ src/ui/routing.py
@@ -4,13 +4,18 @@ Created on Nov 14, 2010
 @author: michalracek
 '''
 
+import urllib
+
 from dbo import UserInfo
 
 def user_id(nick_or_id):
     """
     Finds user by the nickname or id.
     """
-    user = UserInfo.getUserInfoByNick(nick_or_id)
+    #Decode nick from URL format
+    nick_or_id_val = urllib.unquote(nick_or_id)
+    user = UserInfo.getUserInfoByNick(nick_or_id_val)
     if not user:
-        user =  UserInfo.getUserInfoById(nick_or_id)
-    return user
+        user =  UserInfo.getUserInfoById(nick_or_id_val)
+    if len(user)>0:
+        return user[0]
