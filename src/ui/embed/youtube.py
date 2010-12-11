0a3b07c1f364f2b446734a77a32888e6ddc95685
krablak <krablak@7b4a1042-a560-49a6-8a58-85ef7ed48359>
2010-12-11 20:22:41 +0000

    #39
    git-svn-id: http://ays.unfuddle.com/svn/ays_clips/sclip/trunk@202 7b4a1042-a560-49a6-8a58-85ef7ed48359

--- src/ui/embed/youtube.py
+++ src/ui/embed/youtube.py
@@ -22,11 +22,13 @@ def get_embed_content(clip):
     """
     Returns clip embed HTML content.
     """
-    """
-    """
     #Check if url match to youtube video
     if "http://www.youtube.com/watch?v=" in clip.page:
         if clip.page.find("?")!=-1:
-            video_id = clip.page[len("http://www.youtube.com/watch?v="):clip.page.find("&")]
+            video_id = ""
+            if clip.page.find("&")!=-1:
+                video_id = clip.page[len("http://www.youtube.com/watch?v="):clip.page.find("&")]
+            else:
+                video_id = clip.page[len("http://www.youtube.com/watch?v="):]
             return "<object width=\"390\" height=\"290\"><param name=\"movie\" value=\"http://www.youtube.com/v/%s?fs=1&amp;hl=en_US\"></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=\"http://www.youtube.com/v/%s?fs=1&amp;hl=en_US\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"390\" height=\"290\"></embed></object>" % (video_id,video_id)
     return clip.page
