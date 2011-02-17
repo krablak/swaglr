0e3b2ecd8d50f99f17eac6c86252c160b4af1029
krablak <krablak@7b4a1042-a560-49a6-8a58-85ef7ed48359>
2011-02-17 21:15:28 +0000

    Drobne upravena plugina pro ff
    git-svn-id: http://ays.unfuddle.com/svn/ays_clips/sclip/trunk@244 7b4a1042-a560-49a6-8a58-85ef7ed48359

--- other/firefox-extension/swaglr/defaults/preferences/prefs.js
+++ other/firefox-extension/swaglr/defaults/preferences/prefs.js
@@ -1,5 +1,5 @@
-pref("extensions.Swaglr.post.url", "http://localhost:8080/web-api/clip/post/");
-//pref("extensions.Swaglr.post.url", "http://www.swaglr.com/web-api/clip/post/");
+//pref("extensions.Swaglr.post.url", "http://localhost:8080/web-api/clip/post/");
+pref("extensions.Swaglr.post.url", "http://www.swaglr.com/web-api/clip/post/");
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.swaglr@swaglr.com.description", "chrome://Swaglr/locale/overlay.properties");
